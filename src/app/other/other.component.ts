59824ccfaa21301a90c3b5716d4e23849d95d55c
nirmaldavis <nirmal.davis@gmail.com>
2016-12-10 14:54:18 +0530

    Using ViewEncapsulation ViewEncapsulation none will remove the encapsulations and styles will become global

--- src/app/other/other.component.ts
+++ src/app/other/other.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-other',
   templateUrl: './other.component.html',
-  styleUrls: ['./other.component.css']
+  styleUrls: ['./other.component.css'],
+  encapsulation: ViewEncapsulation.None
+
+  
 })
 export class OtherComponent implements OnInit {
 

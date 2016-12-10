617a28050709aba176d5728afcbe053e2d093804
nirmaldavis <nirmal.davis@gmail.com>
2016-12-10 15:10:21 +0530

    Content projection ng-content ng-contnent with selector to nest components and still keep inner content of inner component. Meaning dont replace inner component contents when it get nested in parent component. And ng-contnet used for this, can be use select to narrow / specify from multiple ng-content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,15 @@ import { Component } from '@angular/core';
   //templateUrl: './app.component.html',
   template: `
     <h1>Updated Inline template</h1>
-    <app-other></app-other>
+    <app-other>
+      <section>Section Content</section>
+      <div class="class-select">
+        div with .class-select
+      </div>
+      <footer>Footer content</footer>
+      <header> Header Content </header>
+      
+    </app-other>
     <app-another></app-another>
   `,
   // styleUrls: ['./app.component.css']
